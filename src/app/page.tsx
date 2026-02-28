3626dfe430178e0ca16732145f6501feb99c36ed
CODAS <codas0327@gmail.com>
2026-02-28 22:35:19 +0800

    新建项目

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-sm font-medium tracking-tight">My Window</h1>
+    </div>
+  );
+}
